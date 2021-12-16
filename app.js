ef8b328fb9cdbf68d770b1bd9eeecfe14ef98c04
Noam Shani <Noams156@gmail.com>
2021-12-16 18:12:52 +0100

    server settings

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const { isAuthenticated } = require('./middleware/jwt.js')
 
 const app = express();
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
@@ -37,6 +40,10 @@ app.use("/user", isAuthenticated, user);
 
 // include middleWare
 
+app.use((req, res) => {
+    // If no routes match, send them the React HTML.
+    res.sendFile(__dirname + "/client/build/index.html");
+  });
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
